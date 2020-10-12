6c050dabced04abbe3cca618bb0197e839a823ba
desert3agle <harsh.vardhan320@gmail.com>
2020-10-12 15:36:34 +0530

    Updates

---
+++ precache-manifest.0602b781e93e2872b67303f09d655b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d65e2da5a7443b2e8a12d1c8e2e37b",
+    "url": "/react-basic-states-and-props/index.html"
+  },
+  {
+    "revision": "18194474f92a03b27f15",
+    "url": "/react-basic-states-and-props/static/css/main.c6494b84.chunk.css"
+  },
+  {
+    "revision": "9f37d3e5ced07d270af2",
+    "url": "/react-basic-states-and-props/static/js/2.a72a2aa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-states-and-props/static/js/2.a72a2aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18194474f92a03b27f15",
+    "url": "/react-basic-states-and-props/static/js/main.45215c8e.chunk.js"
+  },
+  {
+    "revision": "063267b4a8448b728553",
+    "url": "/react-basic-states-and-props/static/js/runtime-main.388ed715.js"
+  }
+]);
